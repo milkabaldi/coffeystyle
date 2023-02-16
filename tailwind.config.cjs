0b48a505f9b27b9be470ce6053f8385bbdc0d506
milkabaldi <justmainc8@gmail.com>
2023-02-16 15:39:16 +0200

    Start new project Coffee Shop

---
+++ tailwind.config.cjs
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: 'rgba(3, 3, 1, 0.9) ;',
+        secondary: "#FDDD3D",
+        dimBlack: "#1C1C1A",
+        dimWhite: "#fff",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
+    },
+  },
+  plugins: [],
+};
