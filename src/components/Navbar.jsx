3d614a7c96578b5961a0e42f47ff9b44c455050f
milkabaldi <justmainc8@gmail.com>
2023-02-19 18:17:32 +0200

    Nav menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,24 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <nav>
+        <div>
+          <a href='/'>CoffeStyle.</a>
+        </div>
+        <ul>
+          <li>Home</li>
+          <li>Our Products</li>
+          <li>Blog</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Styleguide</li>
+        </ul>
+        <div>
+          <img src="" alt="cart" />
+          <span>Cart</span>
+          <span>0</span>
+        </div>
+    </nav>
   )
 }
 
