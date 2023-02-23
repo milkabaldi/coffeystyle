59d7d910e999e912e69433c81a7668f35ee2216c
milkabaldi <justmainc8@gmail.com>
2023-02-23 15:17:29 +0200

    More mugs database

--- src/constants/data.js
+++ src/constants/data.js
@@ -12,11 +12,34 @@ const feautered = [
         price: '$ 99.00 USD',
         markdown: '$60.00'
     },
-    
+
 ]
 
+const moreMugs = [
+    {
+        mug: images.Pink,
+        title: 'Pink Premium Ceramic',
+        price: '$ 99.00 USD',
+    },
+    {
+        mug: images.Pink,
+        title: 'Pink Premium Ceramic',
+        price: '$ 99.00 USD',
+    },
+    {
+        mug: images.Pink,
+        title: 'Pink Premium Ceramic',
+        price: '$ 99.00 USD',
+    },
+    {
+        mug: images.Pink,
+        title: 'Pink Premium Ceramic',
+        price: '$ 99.00 USD',
+    }
+]
 
 export default {
     feautered,
+    moreMugs,
 }
 
--- src/constants/images.js
+++ src/constants/images.js
@@ -2,7 +2,15 @@ import Cart   from '../assets/cart.png'
 import BgMugs from '../assets/bg-coffeeMugs.jpg'
 import Golden from '../assets/goldenPremium.jpg'
 import Pink   from '../assets/pinkPremium.jpg'
-
+import CoffeeOne from '../assets/coffeeOne.jpg'
+import CoffeeTwo from '../assets/coffeeTwo.jpg'
+import CoffeeThree from '../assets/coffeeThree.jpg'
+import CoffeeFour from '../assets/coffeeFour.jpg'
+import CoffeeFive from '../assets/coffeeFive.jpg'
+import CoffeeSix from '../assets/coffeeSix.jpg'
+import CoffeeSeven from '../assets/coffeeSeven.jpg'
+import CoffeeEight from '../assets/coffeeEight.jpg'
+import CoffeeNine from '../assets/coffeeNine.jpg'
 
 
 export default {
@@ -10,4 +18,13 @@ export default {
     BgMugs,
     Golden,
     Pink,
+    CoffeeOne,
+    CoffeeTwo,
+    CoffeeThree,
+    CoffeeFour,
+    CoffeeFive,
+    CoffeeSix,
+    CoffeeSeven,
+    CoffeeEight,
+    CoffeeNine,
 }
