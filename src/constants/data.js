483ac90df98d4b28fad63347f347409154cb68e8
milkabaldi <justmainc8@gmail.com>
2023-02-24 20:54:43 +0200

    Mugs cards

--- src/constants/data.js
+++ src/constants/data.js
@@ -17,25 +17,54 @@ const feautered = [
 
 const moreMugs = [
     {
-        mug: images.Pink,
+        mug: images.CoffeeOne,
+        title: 'Red Love Cup',
+        price: '$ 37.00 USD',
+        markdown: '$25.00'
+    },
+    {
+        mug: images.CoffeeTwo,
+        title: 'Black Tea Cup',
+        price: '$ 29.00 USD',
+        markdown: '$15.00'
+    },
+    {
+        mug: images.CoffeeThree,
+        title: 'B&W Essentials Mug',
+        price: '$ 19.00 USD',
+    },
+    {
+        mug: images.CoffeeFour,
+        title: 'Winter Style Mug',
+        price: '$ 25.00 USD',
+    },
+    {
+        mug: images.CoffeeFive,
+        title: 'Ceramic Tea',
+        price: '$ 46.00 USD',
+    },
+    {
+        mug: images.CoffeeSix,
+        title: 'No Handle Bar Cup',
+        price: '$ 34.00 USD',
+    },
+    {
+        mug: images.CoffeeSeven,
+        title: 'Espresso Cup by Mugs.co',
+        price: '$ 25.00 USD',
+    },
+    {
+        mug: images.CoffeeEight,
         title: 'Pink Premium Ceramic',
         price: '$ 99.00 USD',
     },
+ 
     {
-        mug: images.Pink,
-        title: 'Pink Premium Ceramic',
-        price: '$ 99.00 USD',
+        mug: images.CoffeeNine,
+        title: 'Summer Designer Cup',
+        price: '$ 29.00 USD',
     },
-    {
-        mug: images.Pink,
-        title: 'Pink Premium Ceramic',
-        price: '$ 99.00 USD',
-    },
-    {
-        mug: images.Pink,
-        title: 'Pink Premium Ceramic',
-        price: '$ 99.00 USD',
-    }
+ 
 ]
 
 export default {
